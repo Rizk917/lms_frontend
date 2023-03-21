85e92213e7272666b15fd0e1f578ce9786eca7be
fatima <fatimabittar25@gmail.com>
2023-03-21 10:06:48 +0200

    merge

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "./first.css";
 
-
-
-
 function Header() {
-const Role=localStorage.getItem("Role");
+  const Role = localStorage.getItem("Role");
 
-  // {Role==="Admin"?<div className="sidebar-buttons" id="sidebar-buttons"><img src={adminlogo} alt="" /> <p ><Link to='/admin'>Admins</Link></p></div >:null}
-return(
-<>
-   {Role==="Admin"?<div className="header"><h1 className="heading">SuperAdmin Dashboard</h1></div>:<div className="header"><h1 className="heading">Dashboard</h1></div>}
-  
-  
-   </>
-   );
+  return (
+    <>
+      {Role === "Admin" ? (
+        <div className="header">
+          <h1 className="heading">SuperAdmin Dashboard</h1>
+        </div>
+      ) : (
+        <div className="header">
+          <h1 className="heading">Dashboard</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Header;
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,45 +1,82 @@
 import React from "react";
 import "./first.css";
-import { Link, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 import AddAminPage from "../pages/admin-pages/AdminPage";
 import AddClassesPage from "../pages/AddClassesPage";
 import AddSectionsPage from "../pages/AddSectionsPage";
 import AddStudentsPage from "../pages/student-pages/AddStudentsPage";
 import home from "../images/home_icon.png";
-import logo from "../images/logo.png"
-import classlogo from "../images/classicon.png"
-import studentlogo from "../images/studentlogo.png"
-import Attendancelogo from "../images/Attendance.png"
-import adminlogo from "../images/adminlogo.png"
+import logo from "../images/logo.png";
+import classlogo from "../images/classicon.png";
+import studentlogo from "../images/studentlogo.png";
+import Attendancelogo from "../images/Attendance.png";
+import adminlogo from "../images/adminlogo.png";
 
-const Role=localStorage.getItem("Role");
+const Role = localStorage.getItem("Role");
 const Logout = () => {
   window.location.href = "/";
 
   window.localStorage.clear();
-  localStorage.removeItem('token');
+  localStorage.removeItem("token");
 };
 function Sidebar() {
-  return <div className="sidebar-container">
-    <img className="logoo" src={logo} alt="" />
+  return (
+    <div className="sidebar-container">
+      <img className="logoo" src={logo} alt="" />
 
-    <div className="sidebar-buttons"><img src={home} alt="" /> <p><Link to="/home">Home</Link></p></div>
-    <div className="sidebar-buttons"><img src={classlogo} alt="" /> <p ><Link to='/classes'>Classes</Link></p></div>
-    <div className="sidebar-buttons"><img src={studentlogo} alt="" /> <p ><Link to='/students'>Students</Link></p> </div >
-    <div className="sidebar-buttons" id="sidebar-buttons"><img src={Attendancelogo} alt="" /> <p ><Link to='/attendance'>Attendance</Link></p></div >
-  
-  {Role==="Admin"?<div className="sidebar-buttons" id="sidebar-buttons"><img src={adminlogo} alt="" /> <p ><Link to='/admin'>Admins</Link></p></div >:null}
-  <div className="sidebar-buttons" id="sidebar-buttons"><img src={adminlogo} alt="" /> <p ><Link to='/admin'>Admins</Link></p></div >
-    
-    <div href="/" onClick={Logout} className="sidebar-buttons" id="sidebar-buttons"><img src={adminlogo} alt="" /> <p ><Link to='/'>Logout</Link></p></div >
-  </div >
+      <div className="sidebar-buttons">
+        <img src={home} alt="" />{" "}
+        <p>
+          <Link to="/home">Home</Link>
+        </p>
+      </div>
+      <div className="sidebar-buttons">
+        <img src={classlogo} alt="" />{" "}
+        <p>
+          <Link to="/classes">Classes</Link>
+        </p>
+      </div>
+      <div className="sidebar-buttons">
+        <img src={studentlogo} alt="" />{" "}
+        <p>
+          <Link to="/students">Students</Link>
+        </p>{" "}
+      </div>
+      <div className="sidebar-buttons" id="sidebar-buttons">
+        <img src={Attendancelogo} alt="" />{" "}
+        <p>
+          <Link to="/attendance">Attendance</Link>
+        </p>
+      </div>
 
+      {Role === "Admin" ? (
+        <div className="sidebar-buttons" id="sidebar-buttons">
+          <img src={adminlogo} alt="" />{" "}
+          <p>
+            <Link to="/admin">Admins</Link>
+          </p>
+        </div>
+      ) : null}
+      <div className="sidebar-buttons" id="sidebar-buttons">
+        <img src={adminlogo} alt="" />{" "}
+        <p>
+          <Link to="/admin">Admins</Link>
+        </p>
+      </div>
 
-
-
-
-
-
+      <div
+        href="/"
+        onClick={Logout}
+        className="sidebar-buttons"
+        id="sidebar-buttons"
+      >
+        <img src={adminlogo} alt="" />{" "}
+        <p>
+          <Link to="/">Logout</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
